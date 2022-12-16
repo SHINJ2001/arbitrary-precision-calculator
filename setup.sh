a027b56a04ec9e213e747629872fdb58bdb04539
Shivani Joshi <93652126+SHINJ2001@users.noreply.github.com>
2022-12-16 13:46:27 +0530

    Delete setup.sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-echo "# arbitrary-precision-calculator" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:SHINJ2001/arbitrary-precision-calculator.git
-git push -u origin main
